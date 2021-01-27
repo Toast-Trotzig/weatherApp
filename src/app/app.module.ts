29a1531876f92153a55712b637af85488f25555d
Theo Trotzig <theo.trotzig@gmail.com>
2021-01-27 16:21:31 +0100

    Drag and drop added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,16 @@ import { MainComponent } from './component/main/main.component';
 import {MaterialModule} from './shared/material.module';
 import {Routes, RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 import {UserService} from './services/user.service';
 import { PlaceComponent } from './component/place/place.component';
 import {PlaceGuard} from './component/place/place.guard';
 import { WeathercardComponent } from './component/weathercard/weathercard.component';
+import { DragndropComponent } from './component/dragndrop/dragndrop.component';
 
 const routes: Routes = [
   {path: ':long/:lat', canActivate: [PlaceGuard], component: PlaceComponent},
+  {path: 'dragndrop', component: DragndropComponent},
   {path: '', component: MainComponent},
   {path: '**', redirectTo: ''}
 ];
@@ -24,13 +27,15 @@ const routes: Routes = [
     MainComponent,
     PlaceComponent,
     WeathercardComponent,
+    DragndropComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
+    HttpClientModule,
+    DragDropModule
   ],
   providers: [
     UserService
---
+++ src/app/component/dragndrop/dragndrop.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-dragndrop',
+  templateUrl: './dragndrop.component.html',
+  styleUrls: ['./dragndrop.component.css']
+})
+export class DragndropComponent implements OnInit {
+
+  done: string[] = ['wake up', 'eat breakfast', 'brush teeth'];
+  inprogress: string[] = ['work', 'have lunch', 'afternoon snack'];
+  todo: string[] = ['eat dinner', 'go to bed'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+}
